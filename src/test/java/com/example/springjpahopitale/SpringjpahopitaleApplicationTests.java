c599732e0c42cf7c96d0c3a1b01fc77802177039
FouadALLAOUI <allaoui70fouad71@gmail.com>
2023-12-18 21:03:51 +0100

    Main FCT

---
+++ src/test/java/com/example/springjpahopitale/SpringjpahopitaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springjpahopitale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringjpahopitaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
